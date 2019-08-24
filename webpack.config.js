acdee4affe88e3970a231fb0225095be3ff56bee
val <val.antonini@gmail.com>
2019-08-24 16:28:58 +1000

    webpack change for library output

--- webpack.config.js
+++ webpack.config.js
@@ -45,6 +45,7 @@ module.exports = {
   },
   output: {
     filename: 'index.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    library: "promisethat"
   }
 };
