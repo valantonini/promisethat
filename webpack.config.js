dec98881a1cd70eabf6778f3b9139e75dde60439
val <val.antonini@gmail.com>
2019-09-03 21:40:30 +1000

    changing webpack settings

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
     //library: "promisethat",
-    libraryTarget: 'commonjs',
+    libraryTarget: 'commonjs-module',
   },
   module: {
     rules: [{
