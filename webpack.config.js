e0c3a5c6aa48cfdb81a4a28fd5c98eac2d4eddb2
val <val.antonini@gmail.com>
2019-09-03 21:56:18 +1000

    cleanup webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,6 @@ module.exports = {
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
-    //library: "promisethat",
     libraryTarget: 'commonjs-module',
   },
   module: {
